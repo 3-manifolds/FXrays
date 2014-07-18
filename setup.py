b8775622a94830c2c75b4510e09a583e4842c5d0
Nathan Dunfield <nathan@dunfield.info>
2014-07-18 14:03:45 -0500

    Switched to setuptools because its just that much better

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-from distutils.core import setup, Extension
 import os
+from setuptools import setup, Extension
 
 FXrays = Extension("FXrays", sources = ["FXraysmodule.c", "FXrays.c"],
                    extra_compile_args=["-O3", "-funroll-loops"])
