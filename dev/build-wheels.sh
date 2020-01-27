e80281992e77f874f391c840f8ec017dba9438cc
Nathan Dunfield <nathan@dunfield.info>
2020-01-26 20:07:07 -0600

    still trying to get travis to work

--- dev/build-wheels.sh
+++ dev/build-wheels.sh
@@ -3,17 +3,17 @@ set -e -x
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    "${PYBIN}/pip" install -r /io/dev-requirements.txt
+    "${PYBIN}/pip" install -r /fxrays/dev/requirements.txt
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
 
 # Bundle external shared libraries into the wheels
 for whl in wheelhouse/*.whl; do
-    auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
+    auditwheel repair "$whl" --plat $PLAT -w /fxrays/wheelhouse/
 done
 
 # Install packages and test
 for PYBIN in /opt/python/*/bin/; do
-    "${PYBIN}/pip" install FXrays --no-index -f /io/wheelhouse
+    "${PYBIN}/pip" install FXrays --no-index -f /fxrays/wheelhouse
     #(cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
 done
