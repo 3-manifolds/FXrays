aebf74fc23a9fd565955d7e04da6fd8fb28960ef
Nathan Dunfield <nathan@dunfield.info>
2020-01-26 20:54:03 -0600

    Simplified build-script

--- dev/build-wheels.sh
+++ dev/build-wheels.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 set -e -x
+cd /io
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    "${PYBIN}/pip" install -r /fxrays/dev/dev-requirements.txt
-    "${PYBIN}/pip" wheel /fxrays/ -w wheelhouse/
+    "${PYBIN}/pip" install -r dev/dev-requirements.txt
+    "${PYBIN}/python" setup.py release --install
 done
 
 # Bundle external shared libraries into the wheels
-for whl in wheelhouse/*.whl; do
-    auditwheel repair "$whl" --plat $PLAT -w /fxrays/wheelhouse/
-done
+#for whl in wheelhouse/*.whl; do
+#    auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
+#done
 
 # Install packages and test
-for PYBIN in /opt/python/*/bin/; do
-    "${PYBIN}/pip" install FXrays --no-index -f /fxrays/wheelhouse
-    "${PYBIN}/python" -m FXrays.test
-done
+#for PYBIN in /opt/python/*/bin/; do
+#    "${PYBIN}/pip" install FXrays --no-index -f /io/wheelhouse
+#    "${PYBIN}/python" -m FXrays.test
+#done
